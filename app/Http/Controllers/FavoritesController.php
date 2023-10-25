fcef88462d1e2139fc95618ee2cc4ef380113a9a
Atefe <aa.asgariyan12@gmail.com>
2023-10-25 11:56:33 +0330

    create add to favorites endpoin

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -3,9 +3,99 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\FoodResource;
+use App\Models\Customers;
+use App\Models\Foods;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FavoritesController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try{ 
+            // $customer = Auth::user()->customer;
+            $customerId = 3;
+            $customer = Customers::find($customerId);
+
+            if(!$customer){
+                return response()->json(['error'=>['message'=> 'customer not found']], 404);
+            }
+
+            $favorites = json_decode($customer->favorites);
+            $data = [];
+            foreach($favorites as $favorite){
+                $food = Foods::find($favorite);
+                if(!$food){ 
+                    return response()->json(['error'=>['message'=> 'food not found']],404);
+                }
+                $data[] = new FoodResource($food);
+            }
+            return response()->json(['data'=>$data],200);
+        }catch(\Exception $e){
+            return response()->json(['error'=>['message'=> $e->getMessage()]],500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'foodId'=> 'required|integer|exists:foods,id',
+        ]);
+
+        $foodId = $validatedData['foodId'];
+
+        try{ 
+            // $customer = Auth::user()->customer;
+            $customerId = 3;
+            $customer = Customers::find($customerId);
+
+            if(!$customer){
+                return response()->json(['error'=>['message'=> 'customer not found']], 404);
+            }
+
+            $favorites = json_decode($customer->favorites);
+            if(in_array($foodId, $favorites)){
+                return response()->json(['error'=>['message'=> 'food is already in favorites.']],200);
+            }
+            
+            $favorites[] = $foodId;
+            $customer->favorites = json_encode($favorites);
+            $customer->save();
+            return response()->json(['data'=>$favorites],200);
+         
+        }catch(\Exception $e){
+            return response()->json(['error'=>['message'=> $e->getMessage()]],500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BranchesController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\CustomersController;
+use App\Http\Controllers\FavoritesController;
 use App\Http\Controllers\FoodsController;
 use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\SearchController;
@@ -51,6 +52,8 @@ Route::post('search', [SearchController::class ,'search']);
 Route::get('emptycart/{customerId}', [CartController::class,'empty']);
 
 Route::apiResource('cart', CartController::class);
+
+Route::apiResource('favorites', FavoritesController::class);
 
 Route::apiResource('addresses', AddressesController::class);
 
